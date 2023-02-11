6ac04c844e5ac280a3cb77ce43a786a737073b9b
Noorda Studios <118871349+noordotda@users.noreply.github.com>
2023-02-11 13:16:47 -0500

    delete two obsolete files

--- public/shows.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('../app/config/constants.php');
-include RESSOURCES_PATH . '/views/layout/header.php';
-include RESSOURCES_PATH . '/views/single_show.php';
-include RESSOURCES_PATH . '/views/layout/footer.php';
-
-
-?>
